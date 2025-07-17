a561fb6ba350a8ffb99a505b480d00e08d4bb27d
Anuroopa-123 <128488430+Anuroopa-123@users.noreply.github.com>
2025-07-17 15:32:23 +0530

    Added video and members in homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'topbar', component: TopbarComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'hero', component: HeroComponent },
 ];
