332d79db8f1dd45d1f7ff5b41c60ce39388e7b2d
Anuroopa-123 <128488430+Anuroopa-123@users.noreply.github.com>
2025-07-17 18:00:44 +0530

    Added announcements,committee and footer in homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./header/header.component";
 import { TopbarComponent } from "./topbar/topbar.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HeroComponent } from "./hero/hero.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TopbarComponent, NavbarComponent, HeroComponent],
+  imports: [RouterOutlet, HeaderComponent, TopbarComponent, NavbarComponent, HeroComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { TopbarComponent } from './topbar/topbar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { UnionStateRelationsComponent } from './union-state-relations/union-state-relations.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'topbar', component: TopbarComponent },
   { path: 'navbar', component: NavbarComponent },
   { path: 'hero', component: HeroComponent },
+  { path: 'unionstaterelations', component: UnionStateRelationsComponent },
 ];
