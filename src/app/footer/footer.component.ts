cb6f9cb0236abf290fa5e5060d30587bf64c4e78
Anuroopa-123 <128488430+Anuroopa-123@users.noreply.github.com>
2025-07-18 10:01:09 +0530

    small changes in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
